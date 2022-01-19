d514709e9dccecb98dc6aa1f46d7c207c3c3d030
LDB47 <laurent.cochonneau@gmail.com>
2022-01-19 17:06:38 +0100

    Created 2 get methods in QueryDragons

--- src/dragons/QueryDragons.java
+++ src/dragons/QueryDragons.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +15,12 @@ import java.util.List;
  */
 public class QueryDragons {
 
+    /**
+     * 
+     * @return all dragons from database
+     * @throws SQLException 
+     */
+    
     public static List<Dragon> getAll() throws SQLException {
 
         List<Dragon> dragons = new ArrayList<>();
@@ -37,10 +44,40 @@ public class QueryDragons {
             };
         } catch (Exception e) {
             System.err.println(
-                    "Erreur d'affichage d'ing: " + e.getMessage()
+                    "Erreur d'affichage de dragons: " + e.getMessage()
             );
         }
         return dragons;
+    }
+    
+    public static Dragon getDragon() throws SQLException {
+        Dragon dragon = null;
+        Scanner scanner = new Scanner(System.in);
+        String nom = scanner.nextLine();
+
+        try {
+            String query = "SELECT * FROM dragons WHERE dragon = ?;";
+            PreparedStatement declaration = accessDataBase.prepareStatement(query);
+            declaration.setString(1, nom);
+            ResultSet resultat = declaration.executeQuery(query);
+
+            while (resultat.next()) {
+                Dragon dra = new Dragon();
+                dra.setId_dragon(resultat.getInt("id_dragon"));
+                dra.setDragon(resultat.getString("dragon"));
+                dra.setSexe(resultat.getString("sexe"));
+                dra.setLongueur(resultat.getInt("longueur"));
+                dra.setNombre_ecailles(resultat.getInt("nombre_ecailles"));
+                dra.setCrache_feu(resultat.getString("crache_feu"));
+                dra.setComportement_amoureux(resultat.getString("comportement_amoureux"));
+                System.out.println(dra.toString());
+            };
+        } catch (Exception e) {
+            System.err.println(
+                    "Erreur d'affichage de dragon: " + e.getMessage()
+            );
+        }
+        return dragon;
     }
     
     
