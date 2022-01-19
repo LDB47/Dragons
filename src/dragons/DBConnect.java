32701d1bbad3d334b365965cfb562d044d937b4d
LDB47 <laurent.cochonneau@gmail.com>
2022-01-19 16:43:24 +0100

    First Commit - Creation of the classes structures

---
+++ src/dragons/DBConnect.java
@@ -0,0 +1,75 @@
+package dragons;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author laure
+ */
+public class DBConnect {
+
+    static Connection accessDataBase = null;
+
+    public static void openConnection() {
+        /* Parametres de connexion */
+        String url = "jdbc:mysql://127.0.0.1/java_dragons2";
+// nesti = nom de ma bdd
+        String utilisateur = "root";
+        String motDePasse = "";
+        try {
+            System.out.println("Connexion réussie!");
+// on ajoute nos paramètres
+            accessDataBase = DriverManager.getConnection(
+                    url, utilisateur, motDePasse);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+    }
+
+    public static boolean testConnection() {
+        boolean flag = false;
+        try {
+            if (accessDataBase != null) {
+                if (!accessDataBase.isClosed()) {
+                    System.out.println("Connexion au serveur... OK");
+                    flag = true;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+        return flag;
+    }
+
+    public static void closeConnection() {
+        if (accessDataBase != null) {
+            try {
+                accessDataBase.close();
+                System.out.println("Close connection");
+            } catch (SQLException e) {
+                System.err.println(
+                        "Erreur fermeture: " + e.getMessage()
+                );
+            }
+        }
+    }
+    
+//    public static Connection connect() {
+//        Connection conn = null;
+//
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/java_dragons2", "root", "");
+//            System.out.println("Connexion réussie !");
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.out.println(e);
+//        }
+//        return conn;
+//    }
+}
---
+++ src/dragons/Main.java
@@ -0,0 +1,16 @@
+package dragons;
+
+/**
+ *
+ * @author laure
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
